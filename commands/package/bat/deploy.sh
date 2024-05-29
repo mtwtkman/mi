21da6640deafa8f7c41bd8453f1900e3b09b29a4
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-05-29 21:19:05 +0900

    fix something

--- commands/package/bat/deploy.sh
+++ commands/package/bat/deploy.sh
@@ -5,8 +5,9 @@ config_location=$(sh "${here}/../../config/location.sh")
 cp -a "${here}/d/" "${config_location}/bat/"
 
 mkdir -p "$(bat --config-dir)/themes"
-wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
-wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
-wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
-wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+declare -a themes=("Latte" "Frappe" "Macchiato" "Mocha")
+for theme in "${themes[@]}"
+do
+  curl "https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20${theme}.tmTheme" -O --output-dir "$(bat --config-dir)/themes"
+done
 bat cache --build
