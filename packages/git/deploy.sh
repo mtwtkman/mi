09d825c4e54c0c7cb0e1a73ab98d004531c22a6a
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-06-06 14:56:58 +0900

    fixed git deploy

--- packages/git/deploy.sh
+++ packages/git/deploy.sh
@@ -3,5 +3,5 @@ abshere="$(readlink -m ${here})"
 
 file_helper="${here}/../../module/helper/file"
 dest="${HOME}/.config/git"
-sh "file_helper}/create_directory_if_necessary.sh" "${dest}"
+sh "${file_helper}/create_directory_if_necessary.sh" "${dest}"
 sh "${file_helper}/refresh_link.sh" "${abshere}/config" "${dest}/config"
