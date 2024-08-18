1622f880eab7da74af8f8a93d833e4c3dd6fb819
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-08-18 09:21:23 +0900

    improve

--- modules/make_link.sh
+++ modules/make_link.sh
@@ -1,6 +1,15 @@
+here="$(readlink -m $(dirname ${BASH_SOURCE[0]}))"
+
+source "${here}/message.sh"
+
 src=$1
 dest=$2
 if [ -L "${dest}" ]; then
   unlink "${dest}"
+fi
+if [ -e "${dest}" ]; then
+  backup="${dest}.mi.bk"
+  mv "${dest}" "${backup}"
+  yellow "Create backup file for ${dest} as ${backup}"
 fi
 ln -s "${src}" "${dest}"
