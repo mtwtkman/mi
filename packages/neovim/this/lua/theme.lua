3ad507bfc6af5566e23fd903d81d64764681fdff
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-10-23 12:58:38 +0900

    white

--- packages/neovim/this/lua/theme.lua
+++ packages/neovim/this/lua/theme.lua
@@ -21,6 +21,7 @@ hl("IncSearch", c.black, c.light_grey, false)
 
 -- nvim-tree
 hl("Directory", c.white, nil, false)
+hl("NvimTreeFolderIcon", c.white, nil, false)
 
 -- nvim-cmp
 hl("CmpItemAbbrDefault", c.white, nil, false)
@@ -39,3 +40,14 @@ hl("NeogitDiffDelete", c.white, c.red, false)
 
 --dropbar
 hl("DropbarIconUIPickPivot", c.white, c.grey, false)
+
+-- devicon
+hl("DevIconLua", c.whte, nil, false)
+hl("DevIconGitIgnore", c.white, nil, false)
+hl("DevIconText", c.white, nil, false)
+hl("DevIconBash", c.white, nil, false)
+hl("DevIconBashrc", c.white, nil, false)
+hl("DevIconBashProfile", c.white, nil, false)
+hl("DevIconRb", c.white, nil, false)
+hl("DevIconPy", c.white, nil, false)
+hl("DevIconHs", c.white, nil, false)
