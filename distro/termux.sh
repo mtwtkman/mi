8aaf4104c4feb0cfec70bbd97c7462d8d4971c6b
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-11-03 13:06:06 +0900

    updated setup script for termux

--- distro/termux.sh
+++ distro/termux.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 here="$(readlink -m $(dirname ${BASH_SOURCE[0]}))"
 
+termerx_conf="${HOME}/.termux"
+
+function setup()
+{
+  if [[ -d "${termerx_conf}" ]]; then
+    return
+  fi
+  mkdir "${termerx_conf}"
+}
+
+function setup_nerdfont()
+{
+  curl -L https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf > "${termerx_conf}/font.ttf"
+}
+
 install_command="
   yes | pkg install \
     bash \
@@ -17,9 +32,9 @@ install_command="
     ripgrep \
     tmux \
 "
-
-update_command="pkg update && yes | pkg upgrade"
+update_command="pkg update && yes | pkg upgrade && $(setup_nerdfont)"
 purge_command="pkg autoclean"
 
-source "${here}/_common.sh"
-perform "${install_command}" "${update_command}" "${purge_command}" "${1}"
+cmd="${1}"
+
+perform "${install_command}" "${update_command}" "${purge_command}" "${cmd}"
