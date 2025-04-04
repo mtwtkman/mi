1d1f7be5a1cb67ce72bea68316c02eca4c8a93a9
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2025-04-04 14:47:37 +0900

    fixed ts_ls

--- packages/neovim/this/lsp/ts_ls.lua
+++ packages/neovim/this/lsp/ts_ls.lua
@@ -1,5 +1,12 @@
 return {
   cmd = { "npx", "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "typescript" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescript.tsx",
+    "typescriptreact",
+  },
   root_marker = { "tsconfig.json", "package.json" },
 }
