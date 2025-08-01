0f6b42b718a3f91a949e1cf5331ec902a52e60b2
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2025-08-02 07:48:35 +0900

    install yay

--- distro/arch.sh
+++ distro/arch.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 here="$(readlink -m $(dirname ${BASH_SOURCE[0]}))"
 
+function install_fakeroot_tcp()
+{
+  currentdir=$(pwd)
+  workdir="/tmp/facroot-build"
+  makepkg --nobuild
+  tar xf fakeroot_*.tar.gz
+  cd fakeroot-*
+
+  ./bootstrap
+  ./configure --prefix=/usr --libdir=/usr/lib/fakeroot --with-ipc=tcp
+  make
+  sudo make install
+
+  sudo su -
+  cd $workdir
+  cd fakeroot-*
+  source PKGBUILD
+  package
+  exit
+  cd $currentdir
+  /bin/rm -rf $workdir
+}
+
+function install_yay()
+{
+  currentdir=$(pwd)
+  sudo pacman -Sy --noconfirm --needed base-devel
+  workdir="/tmp/aur_build"
+  mkdir $workdir
+  cd $workdir
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+  cd $currentdir
+  /bin/rm -rf $workdir
+}
+
 install_command="
-  yes | sudo pacman -Sy \
+  sudo pacman -Sy --noconfirm \
     bash \
     bash-completion \
     bash-language-server \
@@ -26,9 +63,11 @@ install_command="
     tmux \
     tree-sitter \
     unzip
+  eval $(install_fakeroot_tcp)
+  eval $(install_yay)
 "
-update_command="yes | sudo pacman -Syu"
-purge_command="pacman -Qqtd | sudo pacman -Rs -"
+update_command="yay -Syu --noconfirm"
+purge_command="yay -Yc"
 
 source "${here}/_common.sh"
 perform "${install_command}" "${update_command}" "${purge_command}" "${1}"
