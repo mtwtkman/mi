ebada593fa72cdf7629c5fa9fa576b2f4951268f
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2025-08-21 15:03:43 +0900

    setup grapple

--- packages/neovim/this/lua/plugins/grapple.lua
+++ packages/neovim/this/lua/plugins/grapple.lua
@@ -4,3 +4,9 @@ local grapple = require("grapple")
 utils.nmap("<leader>mg", grapple.toggle)
 utils.nmap("<leader>mn", function() grapple.cycle_tags("next") end)
 utils.nmap("<leader>mp", function() grapple.cycle_tags("prev") end)
+
+grapple.setup({
+  statusline = {
+    icon = "G",
+  },
+})
