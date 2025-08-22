be2948313377053e12274a3c7b983f12016ae206
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2025-08-22 15:48:20 +0900

    removed icon setting from dropbar

--- packages/neovim/this/lua/plugins/dropbar.lua
+++ packages/neovim/this/lua/plugins/dropbar.lua
@@ -1,5 +1,20 @@
 local nmap = require("utils").nmap
+local dropbar = require("dropbar")
 
 vim.ui.select = require("dropbar.utils.menu").select
 
 nmap("<leader><space>", require("dropbar.api").pick, { silent = true })
+
+dropbar.setup({
+  icons = {
+    enable = false,
+    ui = {
+      menu = {
+        indicator = '> ',
+      },
+      bar = {
+        separator = '> ',
+      },
+    },
+  },
+})
