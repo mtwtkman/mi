ffed7f5a442f9a429b07080104a6ae7aa57283ec
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2026-01-11 20:10:55 +0900

    works well

--- packages/neovim/this/lua/netrw.lua
+++ packages/neovim/this/lua/netrw.lua
@@ -1,17 +1,18 @@
 local nmap = require("utils").nmap
 
+vim.g.netrw_keepdir = 0
+vim.g.netrw_autochdir = 1
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 0
 vim.g.netrw_altv = 1
 vim.g.netrw_winsize = 20
 vim.g.netrw_localrmdir = "rmdir"
-vim.api.nvim_create_autocmd('filetype', {
-  pattern = 'netrw',
-  callback = function()
-    nmap('a', '%', { buffer = true, remap = true })
-  end
-})
+nmap("a", "%", { buffer = true, remap = true })
+nmap("q", "<cmd>Lexplore<CR>", { buffer = true, silent = true, nowait = true })
+nmap("l", "<CR>", { buffer = true, remap = true, nowait = true })
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
 local function toggle_netrw()
   if vim.bo.filetype == "netrw" then
     vim.cmd("Rexplore")
@@ -19,5 +20,4 @@ local function toggle_netrw()
     vim.cmd("Lexplore")
   end
 end
-
 nmap("<leader>n", toggle_netrw, { silent = true })
