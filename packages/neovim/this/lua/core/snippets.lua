51b899784ddc489991e77e9433e40268ddf78cb2
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2026-02-04 05:21:18 +0900

    fixed vim.snippet format

--- packages/neovim/this/lua/core/snippets.lua
+++ packages/neovim/this/lua/core/snippets.lua
@@ -1,9 +1,11 @@
 return {
   ruby = {
-    pry = "require 'pry';binding.pry",
-    byebug = "require 'byebug;byebug",
+    fsl = "# frozen_string_literal: true\n$0",
+    rubo = "# rubocop:disable $0",
+    dbg = "require 'debug';binding.break\n$0",
+    deb = "require 'debug';binding.break\n$0",
   },
   python = {
-    pdb = "import pdb;pdb.set_trace();",
+    pdb = "import pdb;pdb.set_trace();\n$0",
   },
 }
