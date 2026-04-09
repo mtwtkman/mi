1e1919570abcb4a3cef36768904255658efb794d
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2026-04-10 05:26:07 +0900

    Refactor init.lua to remove lazy.nvim setup

--- packages/neovim/this/init.lua
+++ packages/neovim/this/init.lua
@@ -1,18 +1,6 @@
 if vim.loader then
   vim.loader.enable()
 end
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
 require("plugins")
 require("core.options")
 require("core.terminal")
