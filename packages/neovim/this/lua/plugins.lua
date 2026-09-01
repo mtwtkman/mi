86e40334d18859f70cc9e390af12029ae04d410f
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2026-09-01 16:32:09 +0900

    vim pack

--- packages/neovim/this/lua/plugins.lua
+++ packages/neovim/this/lua/plugins.lua
@@ -168,7 +168,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
       return
     end
     vim.pack.add({
-      { src = github("mrcjkb/haskell-tools.nvim"), version = vim.version.range("^9") },
+      { src = github("mrcjkb/haskell-tools.nvim"), version = vim.version.range("^10") },
     })
     local lsp_config = require("lsp.config").make_default()
     vim.g.haskell_tools = {
